b94fceac2112a9d23aef8eb33fccbe4857ee3ea9
kellar <blakekellar@gmail.com>
2019-03-09 10:48:25 -0600

    Adds kafka consumer

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 ```
 ./gradlew clean build
 java -jar kafka_producer/build/libs/kafka_producer-0.0.1-SNAPSHOT.jar
+java -jar kafka_consumer_rsocket_producer/build/libs/kafka_consumer_rsocket_producer-0.0.1-SNAPSHOT.jar --spring.confilocation=kafka_consumer_rsocket_producer/application.yml 
 ```
--- kafka_consumer_rsocket_producer/src/main/kotlin/com/blakekellar/rsocketproducer/RsocketProducer.kt
+++ kafka_consumer_rsocket_producer/src/main/kotlin/com/blakekellar/rsocketproducer/RsocketProducer.kt
@@ -1,11 +1,58 @@
 package com.blakekellar.rsocketproducer
 
+import mu.KotlinLogging
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.messaging.handler.annotation.Headers
+import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.stereotype.Component
+
 
 @SpringBootApplication
 class RsocketProducer
 
 fun main(args: Array<String>) {
-	runApplication<RsocketProducer>(*args)
+    runApplication<RsocketProducer>(*args)
+}
+
+@Configuration
+@EnableKafka
+class AppBeans {
+
+    @Bean
+    fun consumerFactory(): ConsumerFactory<String, String> {
+        val props = mutableMapOf<String, Any>()
+        props[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = "localhost:9092"
+        props[ConsumerConfig.GROUP_ID_CONFIG] = "group"
+        props[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        props[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        return DefaultKafkaConsumerFactory(props)
+    }
+
+    @Bean
+    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, String>()
+        factory.consumerFactory = consumerFactory()
+        return factory
+    }
+}
+
+@Component
+class KafkaConsumer() {
+
+    private val logger = KotlinLogging.logger {}
+
+    @KafkaListener(topics = ["topic"], groupId = "group")
+    fun receive(@Payload payload: String, @Headers headers: Map<String, Any>) {
+        logger.info("Received message $payload with headers $headers")
+    }
 }
