5949e8450eb67877ebc0106d0daf90daa1e6c6e8
kellar <blakekellar@gmail.com>
2019-03-14 20:21:14 -0500

    Reactive kafka consumer

--- kafka_consumer_rsocket_client/src/main/kotlin/com/blakekellar/kafkaconsumerrsocketclient/KafkaConsumerRsocketClientApplication.kt
+++ kafka_consumer_rsocket_client/src/main/kotlin/com/blakekellar/kafkaconsumerrsocketclient/KafkaConsumerRsocketClientApplication.kt
@@ -1,6 +1,6 @@
 package com.blakekellar.kafkaconsumerrsocketclient
 
-import io.reactivex.Flowable
+import io.reactivex.disposables.Disposable
 import io.rsocket.kotlin.DefaultPayload
 import io.rsocket.kotlin.RSocket
 import io.rsocket.kotlin.RSocketFactory
@@ -13,100 +13,60 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.kafka.annotation.EnableKafka
-import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
-import org.springframework.kafka.core.ConsumerFactory
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory
-import org.springframework.kafka.support.KafkaHeaders
-import org.springframework.messaging.handler.annotation.Headers
-import org.springframework.stereotype.Component
+import reactor.core.publisher.Flux
+import reactor.kafka.receiver.KafkaReceiver
+import reactor.kafka.receiver.ReceiverOptions
+import reactor.kafka.receiver.ReceiverRecord
+import java.util.*
 
 @SpringBootApplication
 class KafkaConsumerRsocketClientApplication
 
 fun main(args: Array<String>) {
     runApplication<KafkaConsumerRsocketClientApplication>(*args)
+    Thread.currentThread().join()
 }
 
 @Configuration
-@EnableKafka
 class AppBeans {
 
-    @Bean
-    fun consumerFactory(): ConsumerFactory<Any, Any> {
-        val props = mutableMapOf<String, Any>()
-        props[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = "localhost:9092"
-        props[ConsumerConfig.GROUP_ID_CONFIG] = "group"
-        props[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
-        props[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
-        return DefaultKafkaConsumerFactory(props)
-    }
-
-    @Bean
-    fun concurrentKafkaListenerContainerFactory(consumerFactory: ConsumerFactory<Any, Any>): ConcurrentKafkaListenerContainerFactory<String, String> {
-        val factory = ConcurrentKafkaListenerContainerFactory<String, String>()
-        factory.consumerFactory = consumerFactory
-        return factory
-    }
-}
-
-@Component
-class KafkaConsumer(
-        @Autowired val rsocketClient: RsocketClient
-) {
-
     private val logger = KotlinLogging.logger {}
 
-    @KafkaListener(topics = ["topic"], groupId = "group")
-    fun receive(@org.springframework.messaging.handler.annotation.Payload payload: String,
-                @Headers headers: Map<String, Any>) {
-        logger.info("Received kafka message $payload with headers $headers")
-        rsocketClient.exchange(payload, headers[KafkaHeaders.RECEIVED_MESSAGE_KEY] as String?)
+    @Bean
+    fun rSocketClient(): RSocket {
+        return RSocketFactory
+                .connect()
+                .transport(TcpClientTransport.create("127.0.0.1", 4242))
+                .start()
+                .blockingGet()
     }
-}
 
-@Component
-class RsocketClient {
-
-    private val logger = KotlinLogging.logger {}
-    private val rSocket: RSocket = RSocketFactory
-            .connect()
-            .transport(TcpClientTransport.create("127.0.0.1", 4242))
-            .start()
-            .blockingGet()
-
-    fun exchange(payloadText: String, keyText: String?) {
-        val payload = DefaultPayload.text(payloadText, keyText)
-
-        logger.info(">> FF metadata=${payload.metadataUtf8} data=${payload.dataUtf8}")
-        val ffComplete = rSocket.fireAndForget(payload)
-        ffComplete.subscribe {
-            logger.info("<< FF")
-        }
-
-        logger.info(">> RR metadata=${payload.metadataUtf8} data=${payload.dataUtf8}")
-        val response = rSocket.requestResponse(payload)
-        response.subscribe { it ->
-            logger.info("<< RR metadata=${it.metadataUtf8} data=${it.dataUtf8}")
-        }
-
-        logger.info(">> RS metadata=${payload.metadataUtf8} data=${payload.dataUtf8}")
-        val stream = rSocket.requestStream(payload)
-        stream.subscribe { it ->
-            logger.info("<< RS metadata=${it.metadataUtf8} data=${it.dataUtf8}")
-        }
-
-        logger.info(">> RC metadata=${payload.metadataUtf8} data=${payload.dataUtf8}")
-        val channel = rSocket.requestChannel(Flowable.just(payload))
-        channel.subscribe { it ->
-            logger.info("<< RC metadata=${it.metadataUtf8} data=${it.dataUtf8}")
-        }
-
-        logger.info(">> MP metadata=${payload.metadataUtf8} data=${payload.dataUtf8}")
-        val metadata = rSocket.metadataPush(payload)
-        metadata.subscribe {
-            logger.info("<< MP metadata=${payload.metadataUtf8} data=${payload.dataUtf8}")
-        }
+    @Bean
+    fun kafkaPublisher(): Flux<ReceiverRecord<String, String>> {
+        val properties = mutableMapOf<String, Any>()
+        properties[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = "localhost:9092"
+        properties[ConsumerConfig.CLIENT_ID_CONFIG] = "clientid"
+        properties[ConsumerConfig.GROUP_ID_CONFIG] = "group"
+        properties[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java // TODO: Int keys
+        properties[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        properties[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "latest"
+        val receiverOptions = ReceiverOptions.create<String, String>(properties).subscription(Collections.singleton("topic"))
+        return KafkaReceiver.create(receiverOptions).receive()
     }
+
+    @Bean
+    fun disposableSubscriptionFromServer(@Autowired rSocket: RSocket,
+                                         @Autowired kafkaPublisher: Flux<ReceiverRecord<String, String>>): Disposable {
+        return rSocket.requestChannel(
+                kafkaPublisher
+                        .map { it ->
+                            val payload = DefaultPayload.text(it.value(), it.key().toString())
+                            logger.info(">> metadata=${payload.metadataUtf8} data=${payload.dataUtf8}")
+                            payload
+                        })
+                .subscribe { it ->
+                    logger.info("<< metadata=${it.metadataUtf8} data=${it.dataUtf8}")
+                }
+    }
+
 }
